3c85b03d4d2041dd03305b6308630cfc22f0ee01
Ryan Dooley <Rdoolz51@aol.com>
2022-07-01 14:17:37 -0400

    setup api to dynamically call user repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,12 @@
-getUserRepos();
+getUserRepos("rdoolz51");
 
-function getUserRepos() {
-  fetch("https://api.github.com/users/octocat/repos");
+function getUserRepos(user) {
+  var apiURL = "https://api.github.com/users/" + user + "/repos";
+
+  //make request to URL
+  fetch(apiURL).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
 }
